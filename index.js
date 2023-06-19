3b2fcca204cab71e8749e95c1869eaf51fb46e41
Elizaveta Shkimbova <veta-whale@yandex.ru>
2023-06-19 13:36:04 +0300

    added structure and functional app

---
+++ index.js
@@ -0,0 +1,18 @@
+const NEW_TITLE = "Ура, теперь не скучно 🔥";
+
+const wrapper = document.querySelector(".container");
+const titleNode = document.querySelector(".js-title");
+const textNode = document.querySelector(".js-text");
+const buttonNode = document.querySelector(".js-button");
+
+buttonNode.addEventListener("click", function () {
+  fetch("http://www.boredapi.com/api/activity/")
+    .then((response) => response.json())
+    .then((res) => {
+      let activ = res.activity;
+
+      textNode.innerHTML = activ;
+    });
+  wrapper.classList.add("container-activ");
+  titleNode.textContent = NEW_TITLE;
+});
